909fbea820c7763d427b01a6a148fcc96539135d
root <root@localhost.localdomain>
2019-09-08 01:55:44 +0530

    Updates

---
+++ precache-manifest.cccbab43e563c45574492fa85a528fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f9ec78db507c66989149bafb47fa23",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "e1dacebe3ca8813a65a6",
+    "url": "/monster-app/static/css/main.c577e4b9.chunk.css"
+  },
+  {
+    "revision": "6464775d49a4fa7cd7ff",
+    "url": "/monster-app/static/js/2.8e574466.chunk.js"
+  },
+  {
+    "revision": "e1dacebe3ca8813a65a6",
+    "url": "/monster-app/static/js/main.8dfaa56e.chunk.js"
+  },
+  {
+    "revision": "3203d9078c0bc1112e68",
+    "url": "/monster-app/static/js/runtime~main.9879ae4a.js"
+  }
+]);
